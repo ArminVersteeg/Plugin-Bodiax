58540ad8408be5832a8dde236b0918713274cc9d
Armin Versteeg <99063140@mydavinci.nl>
2025-07-01 15:25:43 +0200

    Version 2.2.0
    Added functions:
    Header widget area to display profilepicture and a dropdown on click.
    Profile edit shortcode which lets the user edit their nickname, emailadress and profilepicture with a live preview once a new picture has been uploaded but not yet submitted.

--- plugin/bodiax-plugin.php
+++ plugin/bodiax-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Bodiax
  * Description: Custom CRUD's for dealers and representatives, also adds small other functions/widgets. Created for the Bodiax dealerportal.
- * Version: 2.1.3
+ * Version: 2.2.0
  * Author: Armin Versteeg
 */
 

a1627e912b1ab895bdf30ce3065c9d6351653225
Armin Versteeg <99063140@mydavinci.nl>
2025-07-29 11:35:25 +0200

    Seperate file
    Created a seperate file for the duplicate error modal

--- plugin/public/vertegenwoordigers-pagina.php
+++ plugin/public/vertegenwoordigers-pagina.php
@@ -632,35 +632,3 @@ function check_vertegenwoordiger_duplicates($name, $email) {
 		'reason' => implode(' en ', $duplicates) . ' bestaan al'
 	];
 }
-
-// === DUPLICATE ERROR SHORTCODE ===
-function duplicate_modal_shortcode($atts) {
-	// Get and sanitize query string parameters
-	$names = isset($_GET['names']) ? explode('|', sanitize_text_field($_GET['names'])) : [];
-	$emails = isset($_GET['emails']) ? explode('|', sanitize_text_field($_GET['emails'])) : [];
-	$reasons = isset($_GET['reasons']) ? explode('|', sanitize_text_field($_GET['reasons'])) : [];
-	
-	// Build error modal
-	ob_start(); ?>
-	<?php if (!empty($names)) : ?>
-	<div id="error-modal" class="modal-overlay" style="display: none;">
-		<div class="modal-box">
-			<h3>Deze items zijn overgeslagen i.v.m. duplicaties:</h3>
-			<p class="modal-message">
-				<?php foreach ($names as $key => $name): ?>
-					<?php
-						$email = isset($emails[$key]) ? $emails[$key] : '';
-						$reason = isset($reasons[$key]) ? $reasons[$key] : 'al in gebruik';
-					?>
-				<strong><?php echo esc_html($name); ?></strong>
-				<?php if (!empty($email)) echo ' <strong>(' . esc_html($email) . ')</strong>'; ?> is overgeslagen omdat deze <strong><?php echo esc_html($reason); ?></strong>.
-				<?php if ($key < count($names) - 1) echo '<br>'; ?>
-				<?php endforeach; ?>
-			</p>
-			<button id="modal-ok" class="modal-button">OK</button>
-		</div>
-	</div>
-	<?php endif;
-	return ob_get_clean();
-}
-add_shortcode('duplicate_modal', 'duplicate_modal_shortcode');
